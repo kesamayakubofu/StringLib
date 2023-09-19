47af89dabf999757c6021b318713a74d280385f3
rotoyouoi <rotoyouoi@yandex.ru>
2023-09-19 13:48:59 +0500

    Добавьте файлы проекта.

---
+++ ClassLibrary1/StringClass.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringLib
+
+
+{
+    public class StringClass
+    {
+        /// <summary>
+        /// Метод возвращает true, если входная строка является датой в корректном формате, в противном случае возвращает - false. 
+        /// </summary>
+        /// <param name="textString"></param>
+        /// <param name="typeDate"></param>
+        /// <returns></returns>
+        public static bool StringParse(string textString, int typeDate)
+        {
+            DateTime dateresult;
+            if (!DateTime.TryParse(textString, out dateresult))
+            {
+                return false;
+            }
+            if ((typeDate < 1) || (typeDate > 13))
+            {
+                return false;
+            }
+            string[] types =
+                {
+             "YYYY.MM.DD",
+             "YYYY.DD.MM",
+             "MM.DD.YYYY",
+             "YYYY-M-D",
+             "YYYY/M/D",
+             "YYYY.DD.MM",
+             "D.M.YYYY",
+             "D-M-YYYY",
+             "D/M/YYYY",
+             "DD.MM.YYYY",
+             "DD-MM-YYYY",
+             "DD/MM/YYYY",
+             "M/D/YYYY"
+
+            };
+
+
+                return true;
+        }
+
+        }
+
+    
+}
---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,221 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using StringLib;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod] 
+
+        public void StringParse_StringParses_ReturnedTrueVENGRIA()
+        {
+            //Arrange
+            string testString = "2006.05.30";//проверка на венгрию
+            //Act
+            bool result = StringClass.StringParse(testString,1);
+            //Assert
+            Assert.IsTrue(result);
+
+        }
+        [TestMethod]
+
+        public void StringParse_StringParses_ReturnedTruePOLSHASWICIALITVAKANADA()
+        {
+            //Arrange
+            string testString = "2006-05-30";//проверка на нерусских
+            //Act
+            bool result = StringClass.StringParse(testString, 1);
+            //Assert
+            Assert.IsTrue(result);
+
+        }
+        [TestMethod]
+
+        public void StringParse_StringParses_ReturnedTruJOPANIA()
+        {
+            //Arrange
+            string testString = "2006/05/30";//проверка на узкоглазых
+            //Act
+            bool result = StringClass.StringParse(testString, 1);
+            //Assert
+            Assert.IsTrue(result);
+
+        }
+        [TestMethod]
+
+        public void StringParse_StringParses_ReturnedTrueCHINACHINACHINA()
+        {
+            //Arrange
+            string testString = "2006-5-30";//проверка на узкоглазых чинайцев
+            //Act
+            bool result = StringClass.StringParse(testString, 1);
+            //Assert
+            Assert.IsTrue(result);
+
+        }
+        [TestMethod]
+
+        public void StringParse_StringParses_ReturnedTrueTAIWAN()
+        {
+            //Arrange
+            string testString = "2006/5/30";//проверка на узкоглазых чинайцев
+            //Act
+            bool result = StringClass.StringParse(testString, 1);
+            //Assert
+            Assert.IsTrue(result);
+
+        }
+        
+
+       
+        [TestMethod]
+
+        public void StringParse_StringParses_ReturnedTrueCZH()
+        {
+            //Arrange
+            string testString = "30.5.2006";//проверка на узкоглазых чинайцев
+            //Act
+            bool result = StringClass.StringParse(testString, 1);
+            //Assert
+            Assert.IsTrue(result);
+
+        }
+        [TestMethod]
+
+        public void StringParse_StringParses_ReturnedTrueNIGERLANDI()
+        {
+            //Arrange
+            string testString = "30-5-2006";//проверка на узкоглазых чинайцев
+            //Act
+            bool result = StringClass.StringParse(testString, 1);
+            //Assert
+            Assert.IsTrue(result);
+
+        }
+        [TestMethod]
+
+        public void StringParse_StringParses_ReturnedTrueBrazil()
+        {
+            //Arrange
+            string testString = "30/5/2006";//проверка на узкоглазых чинайцев
+            //Act
+            bool result = StringClass.StringParse(testString, 1);
+            //Assert
+            Assert.IsTrue(result);
+
+        }
+        [TestMethod]
+
+        public void StringParse_StringParses_ReturnedTrueNIGERMANIA()
+        {
+            //Arrange
+            string testString = "30.05.2006";//проверка на узкоглазых чинайцев
+            //Act
+            bool result = StringClass.StringParse(testString, 1);
+            //Assert
+            Assert.IsTrue(result);
+
+        }
+        [TestMethod]
+
+        public void StringParse_StringParses_ReturnedTrueDANYA()
+        {
+            //Arrange
+            string testString = "30-05-2006";//проверка на узкоглазых чинайцев
+            //Act
+            bool result = StringClass.StringParse(testString, 1);
+            //Assert
+            Assert.IsTrue(result);
+
+        }
+        [TestMethod]
+
+        public void StringParse_StringParses_ReturnedTrueVIETNAM()
+        {
+            //Arrange
+            string testString = "30/05/2006";//проверка на узкоглазых чинайцев
+            //Act
+            bool result = StringClass.StringParse(testString, 1);
+            //Assert
+            Assert.IsTrue(result);
+
+        }
+        
+
+       
+        [TestMethod]
+
+        public void StringParse_StringParses_ReturnedFalseprobelonly()
+        {
+            //Arrange
+            string testString = " ";//проверка на узкоглазых чинайцев
+            //Act
+            bool result = StringClass.StringParse(testString, 1);
+            //Assert
+            Assert.IsFalse(result);
+
+        }
+        [TestMethod]
+
+        public void StringParse_StringParses_ReturnedFalseEmptyString()
+        {
+            //Arrange
+            string testString = "";//проверка на узкоглазых чинайцев
+            //Act
+            bool result = StringClass.StringParse(testString, 1);
+            //Assert
+            Assert.IsFalse(result);
+
+        }
+        [TestMethod]
+
+        public void StringParse_StringParses_ReturnedTrueMAGDATA()
+        {
+            //Arrange
+            string testString = "01.01.1900";//проверка на узкоглазых чинайцев
+            //Act
+            bool result = StringClass.StringParse(testString, 1);
+            //Assert
+            Assert.IsTrue(result);
+
+        }
+        [TestMethod]
+
+        public void StringParse_StringParses_ReturnedFALSEDATE()
+        {
+            //Arrange
+            string testString = "00.12.2023, 30.02.23";//проверка на узкоглазых чинайцев
+            //Act
+            bool result = StringClass.StringParse(testString, 1);
+            //Assert
+            Assert.IsFalse(result);
+
+        }
+        [TestMethod]
+
+        public void StringParse_StringParses_ReturnedFALSEDATE1()
+        {
+            //Arrange
+            string testString = "12.15.2023";//проверка на узкоглазых чинайцев
+            //Act
+            bool result = StringClass.StringParse(testString, 1);
+            //Assert
+            Assert.IsFalse(result);
+
+        }
+        [TestMethod]
+
+        public void StringParse_StringParses_ReturnedFALSEDATE12()
+        {
+            //Arrange
+            string testString = "12.15.0000 ";//проверка на узкоглазых чинайцев
+            //Act
+            bool result = StringClass.StringParse(testString, 1);
+            //Assert
+            Assert.IsFalse(result);
+
+        }
+    }
+}
